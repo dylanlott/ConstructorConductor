1212cda2af7f4b87c4ed41e407e49fd9c35b85ce
dylanlott <lott.dylan@gmail.com>
2015-04-07 15:57:19 -0600

    constructor conductor

--- instanceArray.js
+++ instanceArray.js
@@ -13,29 +13,40 @@ var User = function(name, email, pw){
 
 //Create an Array called 'users' that will store all our instances of User.
 
-  //code here
+var users = [];
 
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
-  //code here
+var tyler = new User('Tyler', tylermcginnis33@gmail.com, 'iLoveJS')
+users.push(tyler); 
 
-console.log('Tyler\'s information is ');
+var cahlan = new User('Cahlan', cahlan@devmounta.in, 'iLoveHashtags')
+users.push(cahlan); 
+
+var lenny = new User('Lenny', lenny@theLenster.com, 'iLoveLentilSoup')
+users.push(lenny); 
+
+console.log('Tyler\'s information is ' + tyler);
 //Console.log all of Tylers information
 
   //code here
 
-console.log('Lenny\'s information is ');
-//Now console.log all of Lennys information
 
-  //code here
+//Now console.log all of Lennys information
+console.log('Lenny\'s information is ' + lenny);
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
-  //code here
+var dylan = new User('Dylan', lott.dylan@gmail.com, 'dylan123'); 
 
-console.log('All my users names are ');
+
 //Now loop through your users Array and console.log every users name. 
+console.log('All my users names are ' + allUsers());
 
-  //code here
+var listUsers = function(arr){
+  for(i=0; i < arr.length; i++){
+    console.log([i]);
+  }
+}
